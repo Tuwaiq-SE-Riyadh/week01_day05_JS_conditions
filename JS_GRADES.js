ebbf6b9d28711cc42674a77e64512c6fc13cb907
Mohammed <Mohammedalnasser641@gmail.com>
2021-10-07 14:59:49 +0300

    two assignment in one file. grades and even&odd

---
+++ JS_GRADES.js
@@ -0,0 +1,43 @@
+function evenOdd ( x ) {
+
+
+
+
+if ( x % 2 == 0 ){
+    console.log ( "odd" )
+} else
+
+console.log( "even" )
+
+}
+
+
+
+// even&Odd assignment ^ 
+
+
+
+
+function grade ( x ) { 
+ 
+   if ( x >= 90) {
+   console.log( "A" )
+   } else if ( x <= 89 && x >= 80) {
+       console.log ( "B" )
+   }
+  else if ( x <= 79 && x >= 70){
+       console.log ( "C" )
+   }
+   else if ( x <= 69 && x >= 60){
+    console.log ( "D" )
+   }
+
+   else if ( x <= 59 && x >= 50){
+    console.log ( "F" )
+   }
+
+} 
+
+
+
+
